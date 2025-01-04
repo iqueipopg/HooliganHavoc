37fcb4d0c7988c06d4b8c12e921d59030b15a46e
Ignacio <202214125@alu.comillas.edu>
2025-01-04 15:18:53 +0100

    Added player and enemy scripts with preliminary animations and sprites

---
+++ HooliganHavoc/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int maxhealth = 100;
+    [SerializeField] float speed = 100;
+
+    private int currentHealth;
+
+    Animator anim;
+    Transform target;
+
+    private void Start()
+    {
+        currentHealth = maxhealth;
+        target = GameObject.Find("Player").transform;
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        if (target != null)
+        {
+            Vector3 direction = target.position - transform.position;
+            direction.Normalize();
+
+            transform.position += direction * speed * Time.deltaTime;
+
+            var playerToRight = target.position.x > transform.position.x;   
+            transform.localScale = new Vector2(playerToRight ? -1 : 1, 1);
+        }
+
+    }
+
+    public void Hit(int damage)
+    {
+        currentHealth -= damage;
+        anim.SetTrigger("Hit");
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
